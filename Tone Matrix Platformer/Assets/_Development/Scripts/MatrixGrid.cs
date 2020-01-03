ffc00af4f63226403cbd29609760c417e014bf6d
Matt-Rader <raderm@umich.edu>
2020-01-03 00:05:53 -0500

    First commit to repo

---
+++ Tone Matrix Platformer/Assets/_Development/Scripts/MatrixGrid.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatrixGrid : MonoBehaviour
+{
+	public NoteBlock[] noteBlocks;
+	private NoteBlock[,] noteBlockMatrix;
+	private int numNoteBlocks;
+
+	[SerializeField] private float noteBlockPadding = 0.4f;
+
+	[SerializeField] private int _BPM = 90;
+
+	int currentColToPlay = 0;
+	float timeBetweenBeatsCounter = 0.0f;
+
+    void Start()
+    {
+		numNoteBlocks = noteBlocks.Length;
+
+		CreateMatrix();		
+    }
+
+	void CreateMatrix () {
+		noteBlockMatrix = new NoteBlock[numNoteBlocks, numNoteBlocks];
+
+		float startSpwnOffset = 0 - (numNoteBlocks / 2) - (noteBlockPadding * (numNoteBlocks / 2));
+
+		Vector3 blockSpawnPosition = new Vector3(startSpwnOffset, startSpwnOffset, 0.0f);
+
+		for (int x = 0; x < numNoteBlocks; ++x) {
+			for (int y = 0; y < numNoteBlocks; ++y) {
+				noteBlockMatrix[x, y] = Instantiate(noteBlocks[y], blockSpawnPosition, Quaternion.identity);
+				blockSpawnPosition.y += (1.0f + noteBlockPadding);
+			}
+			blockSpawnPosition.x += (1.0f + noteBlockPadding);
+			blockSpawnPosition.y = startSpwnOffset;
+		}
+	}
+
+    void Update()
+    {
+		if (timeBetweenBeatsCounter < (60.0f / _BPM)) {
+			timeBetweenBeatsCounter += Time.deltaTime;
+		}
+		else {
+			PlayEnabledBlocksInCol(currentColToPlay);
+			currentColToPlay = (currentColToPlay + 1) % numNoteBlocks;
+			timeBetweenBeatsCounter = 0.0f;
+		}
+    }
+
+	void PlayEnabledBlocksInCol (int col) {
+		for (int i = 0; i < numNoteBlocks; ++i) {
+			if (noteBlockMatrix[col, i].blockEnabled) {
+				Debug.Log(i);
+				noteBlockMatrix[col, i].PlayAudio();
+			}
+		}
+	}
+}
---
+++ Tone Matrix Platformer/Assets/_Development/Scripts/NoteBlock.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteBlock : MonoBehaviour
+{
+	AudioSource noteAudio;
+
+	SpriteRenderer spriteRenderer;
+	Color nodeBlockEnabled = Color.white;
+	Color nodeBlockDisabled = Color.grey;
+
+	[HideInInspector]
+	public bool blockEnabled = false;
+
+	void Awake () { 
+		noteAudio = GetComponent<AudioSource>();
+		spriteRenderer = GetComponent<SpriteRenderer>();
+
+		spriteRenderer.color = nodeBlockDisabled;
+	}
+
+	void OnMouseOver () {
+		if (Input.GetMouseButtonDown(0)) {
+			blockEnabled = !blockEnabled;
+			spriteRenderer.color = blockEnabled ? nodeBlockEnabled : nodeBlockDisabled;
+		}
+	}
+
+	public void PlayAudio () {
+		if (noteAudio != null) {
+			noteAudio.Play();
+		}
+	}
+}
