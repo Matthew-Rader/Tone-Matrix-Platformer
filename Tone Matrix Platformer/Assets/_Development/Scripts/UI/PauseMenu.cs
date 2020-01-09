84b56f0b59e0640327aa46d0ed0effe19c4eed6c
Matt-Rader <raderm@umich.edu>
2020-01-08 22:58:21 -0500

    Started work on a pause menu

---
+++ Tone Matrix Platformer/Assets/_Development/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+	public static bool _GamePaused = false;
+	[SerializeField] private GameObject pauseMenuUI;
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			if (_GamePaused) {
+				Resume();
+			}
+			else {
+				Pause();
+			}
+		}
+	}
+
+	public void Resume () {
+		_GamePaused = false;
+		pauseMenuUI.SetActive(false);
+		Time.timeScale = 1.0f;
+	}
+
+	void Pause () {
+		_GamePaused = true;
+		pauseMenuUI.SetActive(true);
+		Time.timeScale = 0.0f;
+	}
+}
