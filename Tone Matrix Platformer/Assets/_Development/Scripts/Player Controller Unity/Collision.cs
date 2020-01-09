e9d9cb4334181722ed87a59a907926a9a8046fb0
Matt-Rader <raderm@umich.edu>
2020-01-08 20:26:21 -0500

    Updated how collision bounds are set

--- Tone Matrix Platformer/Assets/_Development/Scripts/Player Controller Unity/Collision.cs
+++ Tone Matrix Platformer/Assets/_Development/Scripts/Player Controller Unity/Collision.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class Collision : MonoBehaviour
 {
 	public CollisionInfo collInfo;
+
+	private Collider2D collider;
 
 	private bool groundedLastFrame = false;
 
@@ -17,31 +20,44 @@ public class Collision : MonoBehaviour
 	[SerializeField] private LayerMask whatIsHazard;
 
 	[Header("Collision check points")]
-	private Vector2 bottomOffset = new Vector2(0f, -0.5f);
-	private Vector2 bottomOverlapBox = new Vector2(0.95f, 0.02f);
-	private Vector2 aboveOffset = new Vector2(0f, 0.5f);
-	private Vector2 aboveOverlapBox = new Vector2(0.95f, 0.02f);
-	private Vector2 leftOffset = new Vector2(-0.5f, 0f);
-	private Vector2 leftOverlapBox = new Vector2(0.02f, 0.95f);
-	private Vector2 rightOffset = new Vector2(0.5f, 0f);
-	private Vector2 rightOverlapBox = new Vector2(0.02f, 0.95f);
+	private Vector2 bottomOffset;
+	private Vector2 aboveOffset;
+	private Vector2 leftOffset;
+	private Vector2 rightOffset;
+	private Vector2 widthOverlapBox;
+	private Vector2 heightOverlapBox;
 
 	[Header("Events")]
 	[Space]
 	public UnityEvent OnLandEvent;
 
-	void Awake()
+	void Start()
 	{
 		if (OnLandEvent == null)
 			OnLandEvent = new UnityEvent();
+
+		collider = GetComponent<BoxCollider2D>();
+
+		Bounds bounds = collider.bounds;
+		float boundsWidth = bounds.size.x;
+		float boundsHeight = bounds.size.y;
+		float overlapBoxWidth = boundsWidth - 0.02f;
+		float overlapBoxHeight = boundsHeight - 0.02f;
+
+		bottomOffset = new Vector2(0.0f, -(bounds.size.y / 2.0f));
+		aboveOffset = new Vector2(0.0f, (bounds.size.y / 2.0f));
+		leftOffset = new Vector2(-(bounds.size.x / 2.0f), 0.0f);
+		rightOffset = new Vector2((bounds.size.x / 2.0f), 0.0f);
+
+		heightOverlapBox = new Vector2(0.02f, overlapBoxHeight);
+		widthOverlapBox = new Vector2(overlapBoxWidth, 0.02f);
 	}
 
-	// Update is called once per frame
 	void FixedUpdate () {
 		bool wasGrounded = collInfo.onGround;
 
 		// Check for ground collision
-		collInfo.onGround = Physics2D.OverlapBox((Vector2)transform.position + bottomOffset, bottomOverlapBox, 0.0f, whatIsGround);
+		collInfo.onGround = Physics2D.OverlapBox((Vector2)transform.position + bottomOffset, widthOverlapBox, 0.0f, whatIsGround);
 
 		if (!collInfo.onGround && groundedLastFrame) {
 			groundedLastFrame = false;
@@ -53,29 +69,28 @@ public class Collision : MonoBehaviour
 			OnLandEvent.Invoke();
 
 		// Check for left wall collision
-		collInfo.onWallLeft = Physics2D.OverlapBox((Vector2)transform.position + leftOffset, leftOverlapBox, 0.0f, whatIsWall);
+		collInfo.onWallLeft = Physics2D.OverlapBox((Vector2)transform.position + leftOffset, heightOverlapBox, 0.0f, whatIsWall);
 
 		// Check for right wall collision
-		collInfo.onWallRight = Physics2D.OverlapBox((Vector2)transform.position + rightOffset, rightOverlapBox, 0.0f, whatIsWall);
+		collInfo.onWallRight = Physics2D.OverlapBox((Vector2)transform.position + rightOffset, heightOverlapBox, 0.0f, whatIsWall);
 
 		collInfo.onWall = collInfo.onWallLeft || collInfo.onWallRight ? true : false;
 
-		collInfo.wallSide = collInfo.onWallRight ? -1 : 1;
-
 		// Check to see if we hit a hazard
-		collInfo.touchedHazard = (Physics2D.OverlapBox((Vector2)transform.position + bottomOffset, bottomOverlapBox, 0.0f, whatIsHazard) ||
-								  Physics2D.OverlapBox((Vector2)transform.position + aboveOffset, aboveOverlapBox, 0.0f, whatIsHazard) ||
-								  Physics2D.OverlapBox((Vector2)transform.position + leftOffset, leftOverlapBox, 0.0f, whatIsHazard) ||
-								  Physics2D.OverlapBox((Vector2)transform.position + rightOffset, rightOverlapBox, 0.0f, whatIsHazard));
+		collInfo.touchedHazard = (Physics2D.OverlapBox((Vector2)transform.position + bottomOffset, widthOverlapBox, 0.0f, whatIsHazard) ||
+								  Physics2D.OverlapBox((Vector2)transform.position + aboveOffset, widthOverlapBox, 0.0f, whatIsHazard) ||
+								  Physics2D.OverlapBox((Vector2)transform.position + leftOffset, heightOverlapBox, 0.0f, whatIsHazard) ||
+								  Physics2D.OverlapBox((Vector2)transform.position + rightOffset, heightOverlapBox, 0.0f, whatIsHazard));
 	}
 
 	void OnDrawGizmos()
 	{
 		Gizmos.color = Color.red;
 
-		Gizmos.DrawWireCube(transform.position + (Vector3)bottomOffset, new Vector3(0.95f, 0.05f, 0.0f));
-		Gizmos.DrawWireCube(transform.position + (Vector3)leftOffset, new Vector3(0.05f, 0.95f, 0.0f));
-		Gizmos.DrawWireCube(transform.position + (Vector3)rightOffset, new Vector3(0.05f, 0.95f, 0.0f));
+		Gizmos.DrawWireCube(transform.position + (Vector3)bottomOffset, widthOverlapBox);
+		Gizmos.DrawWireCube(transform.position + (Vector3)aboveOffset, widthOverlapBox);
+		Gizmos.DrawWireCube(transform.position + (Vector3)leftOffset, heightOverlapBox);
+		Gizmos.DrawWireCube(transform.position + (Vector3)rightOffset, heightOverlapBox);
 	}
 
 	public struct CollisionInfo {
@@ -84,13 +99,11 @@ public class Collision : MonoBehaviour
 		public bool onWallLeft;
 		public bool onWallRight;
 		public bool touchedHazard;
-		public int wallSide;
 		public float timeLeftGround;
 
 		public void Reset () {
 			onGround = onWall = false;
 			onWallLeft = onWallRight = false;
-			wallSide = 0;
 		}
 	}
 }
